fa2b51d9ca6151deca2d6cf23d61d6367bc8fbd5
HS <hasang2691@naver.com>
2024-04-16 20:45:47 +0900

    Feat: Update Register and Login Function

--- src/auth/AuthUI.java
+++ src/auth/AuthUI.java
@@ -1,15 +1,22 @@
 package auth;
 
 import bank.MainMenu;
+
 import database.DatabaseManager;
 import database.UserDao;
 
 import java.util.Scanner;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class AuthUI {
     private RegistrationService registrationService;
     private LoginService loginService;
+    private UserDao userDao;
     private final Scanner scanner;
+    
 
     public AuthUI() {
         this.scanner = new Scanner(System.in);
@@ -18,7 +25,7 @@ public class AuthUI {
 
     private void initializeServices() {
         DatabaseManager dbManager = new DatabaseManager();
-        UserDao userDao = new UserDao(dbManager);
+        userDao = new UserDao(dbManager);
         this.registrationService = new RegistrationService(userDao);
         this.loginService = new LoginService(userDao);
     }
@@ -48,7 +55,10 @@ public class AuthUI {
                     if (loggedInUserId != null) {
                         MainMenu mainMenu = new MainMenu(loggedInUserId);
                         mainMenu.show();
+                    } else {
+//                        System.out.println("로그인 실패");
                     }
+                    break;
                 case "0":
                     System.out.println("서비스를 종료합니다.");
                     return;
@@ -60,6 +70,12 @@ public class AuthUI {
     }
 
     private void registerUser() {
+    	
+    	  String userId = "";
+          String password = "";
+          String name = "";
+          String rrn = "";
+          
         System.out.println("============================================");
         System.out.println("[회원가입 서비스]");
         System.out.println("============================================");
@@ -68,32 +84,42 @@ public class AuthUI {
         // 아이디
         while (true) {
             System.out.print("아이디를 입력하세요: ");
-            String userId = scanner.nextLine();
+            userId = scanner.nextLine();
 
             if (userId == null || userId.isEmpty()) {
                 System.out.println("아이디를 입력해주세요");
+                continue;
+            }
+            
+            else if (!userId.matches("[a-z0-9]+")) {
+                System.out.println("소문자 영어와 숫자의 조합으로 이루어져야 합니다.");
                 continue;
             }
 
             else if (userId.length() < 6 || userId.length() > 10) {
                 System.out.println("6자 이상 10자 이하로 입력해주세요.");
                 continue;
-            }
-
-            else if (!userId.matches("[a-z0-9]+")) {
-                System.out.println("소문자 영어와 숫자의 조합으로 이루어져야 합니다.");
-                continue;
-            }
-            
-            else {
-            	break;
-            }
+            } else
+				try {
+					if(userDao.checkDuplicateUserId(userId)) {
+						System.out.println("아이디가 이미 존재합니다.");
+						continue;
+					}
+					
+					else {
+      
+						break;
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
         }
         
-     // Password input
+     // 비밀번호
         while (true) {
             System.out.print("비밀번호를 입력하세요: ");
-            String password = scanner.nextLine();
+            password = scanner.nextLine();
 
             if (password == null || password.isEmpty()) {
                 System.out.println("비밀번호를 입력해주세요.");
@@ -116,10 +142,14 @@ public class AuthUI {
         // 이름
         while (true) {
             System.out.print("이름을 입력하세요: ");
-            String name = scanner.nextLine();
+            name = scanner.nextLine();
 
             if (name == null || name.isEmpty()) {
                 System.out.println("이름을 입력해주세요.");
+                continue;
+            }
+            else if (!name.matches("[가-힣]+")) {
+                System.out.println("이름은 한글로만 구성되어야 합니다.");
                 continue;
             }
 
@@ -127,12 +157,6 @@ public class AuthUI {
                 System.out.println("2자 이상 5자 이하로 입력해주세요.");
                 continue;
             }
-
-            else if (!name.matches("[가-힣]+")) {
-                System.out.println("이름은 한글로만 구성되어야 합니다.");
-                continue;
-            }
-
             else if (!name.matches("^[ㄱ-ㅎㅏ-ㅣ가-힣]*$")) {
                 System.out.println("이름은 자음과 모음의 조합으로 이루어져야합니다.");
                 continue;
@@ -143,29 +167,73 @@ public class AuthUI {
 
         // 주민등록번호
         while (true) {
-            System.out.print("주민등록번호를 입력하세요: ");
-            String rrn = scanner.nextLine();
+            System.out.print("주민등록번호를 입력하세요: (6자리 숫자 - 7자리 숫자) ");
+            rrn = scanner.nextLine();
 
             if (rrn == null || rrn.isEmpty()) {
                 System.out.println("주민등록번호를 입력해주세요.");
                 continue;
             }
 
-            else if (!rrn.matches("\\d+")) {
-                System.out.println("주민등록번호는 숫자만 포함해야 합니다.");
+            else if (!rrn.matches("\\d{6}-\\d{7}")) {
+                System.out.println("주민등록번호는 (6자리 숫자)-(7자리 숫자) 형식이어야 합니다.");
                 continue;
-            }
+            } else
+				try {
+					if (userDao.checkDuplicateRRN(rrn)) {
+						System.out.println("주민등록번호가 이미 존재합니다.");
+						continue;
+					}
 
-            else if (rrn.length() != 13) {
-                System.out.println("주민등록번호는 13자리 숫자로 이루어져야 합니다.");
-                continue;
-            }
+					else {
+						// 생년월일
+					    String birthDate = rrn.substring(0, 6);
+					    try {
+					        LocalDate.parse(birthDate , DateTimeFormatter.ofPattern("yyMMdd"));
+					    } catch (DateTimeParseException e) {
+					        System.out.println("유효하지 않은 생년월일입니다.");
+					        continue;
+					    }
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            
 
             break;
         }
+
+//        while (true) {
+//            System.out.print("주민등록번호를 입력하세요: (6자리 숫자 - 7자리 숫자) ");
+//            String rrn = scanner.nextLine();
+//
+//            if (rrn == null || rrn.isEmpty()) {
+//                System.out.println("주민등록번호를 입력해주세요.");
+//                continue;
+//            }
+//
+//            else if (!rrn.matches("\\d+")) {
+//                System.out.println("주민등록번호는 숫자만 포함해야 합니다.");
+//                continue;
+//            }
+//
+//            else if (rrn.length() != 13) {
+//                System.out.println("주민등록번호는 13자리 숫자로 이루어져야 합니다.");
+//                continue;
+//            }
+//
+//            break;
+//        }
         
-        System.out.println("회원가입이 완료되었습니다!");
         
+     // 회원가입 정보 추가
+        try {
+            registrationService.registerUser(userId, password, name, rrn);
+            
+        } catch (Exception e) {
+            System.out.println("회원가입 중 오류가 발생했습니다: " + e.getMessage());
+        }
     }
 
     private String loginUser() {
@@ -173,14 +241,21 @@ public class AuthUI {
         System.out.println("[로그인 서비스]");
         System.out.println("============================================");
         System.out.println("로그인을 시작합니다.");
+        
+        String userId;
+        String password;
 
         // 아이디
         while (true) {
             System.out.print("아이디를 입력하세요: ");
-            String userId = scanner.nextLine();
+            userId = scanner.nextLine();
 
             if (userId == null || userId.isEmpty()) {
                 System.out.println("아이디를 입력해주세요");
+                continue;
+            }
+            else if (!userId.matches("[a-z0-9]+")) {
+                System.out.println("소문자 영어와 숫자의 조합으로 이루어져야 합니다.");
                 continue;
             }
 
@@ -189,10 +264,7 @@ public class AuthUI {
                 continue;
             }
 
-            else if (!userId.matches("[a-z0-9]+")) {
-                System.out.println("소문자 영어와 숫자의 조합으로 이루어져야 합니다.");
-                continue;
-            }
+            
 
             break;
         }
@@ -200,7 +272,7 @@ public class AuthUI {
         // 비밀번호
         while (true) {
             System.out.print("비밀번호를 입력하세요: ");
-            String password = scanner.nextLine();
+            password = scanner.nextLine();
 
             if (password == null || password.isEmpty()) {
                 System.out.println("비밀번호를 입력해주세요.");
@@ -226,7 +298,7 @@ public class AuthUI {
             System.out.println("로그인 성공!");
             return userId;
         } else {
-            System.out.println("로그인 실패");
+//            System.out.println("로그인 실패");
             return null;
         }
     }
