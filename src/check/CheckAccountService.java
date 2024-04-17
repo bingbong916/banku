52e57b4e47f32c9a3b5c21347f88097851a49471
Hwang-hye-ryeong <dr4853@naver.com>
2024-04-17 14:26:23 +0900

    입력값 오류 처리

--- src/check/CheckAccountService.java
+++ src/check/CheckAccountService.java
@@ -20,6 +20,9 @@ public class CheckAccountService {
         UserDao userDao = new UserDao(dbManager);
         AccountDao accountDao = new AccountDao(dbManager);
     }
+    private static boolean isValidAccountNumber(String accountNumber) {
+        return accountNumber.matches("\\d{6}-\\d{7}");
+    }
 
     
     public static void checkAccount(String loggedInUserId) {
@@ -37,28 +40,59 @@ public class CheckAccountService {
             System.out.println("\n\n[계좌 조회 서비스]");
             System.out.println("============================================");
             System.out.println("('q를 입력할 시 이전 화면으로 돌아갑니다.')");
-            System.out.print("이름을 입력하세요: ");
-            String userName = scan.nextLine();
-            if(!(userName.equals(storedName))) {
-                System.out.println("존재하지 않는 이름입니다.");
-                return;
+
+            String userName;
+            String userRRN;
+            while(true) {
+                System.out.print("이름을 입력하세요: ");
+                userName = scan.nextLine();
+                if(userName.equals("q")) {
+                    System.out.println("이전으로 돌아갑니다.");
+                    return;
+                }
+                if(!userName.matches("^[가-힣]*$")){
+                    System.out.println("이름의 형식이 잘못되었습니다.");
+                    continue;
+                }
+                if(userName.length() < 2 || userName.length() > 3){
+                    System.out.println("이름의 길이가 잘못되었습니다.");
+                    continue;
+                }
+                if(!(userName.equals(storedName))) {
+                    System.out.println("존재하지 않는 이름입니다.");
+                    return;
+                }
+                else break;
             }
-            System.out.print("주민번호를 입력하세요(입력 예시: 991106-010101): ");
-            String userRRN = scan.nextLine();
+
+            while(true) {
+                System.out.print("주민번호를 입력하세요(입력 예시: 991106-010101): ");
+                userRRN = scan.nextLine();
+                if(userRRN.equals("q")) {
+                    // checkManager.printCheckingMenu(loggedInUserId);
+                    System.out.println("이전으로 돌아갑니다.");
+                    return;
+                }
+                if(!isValidAccountNumber(userRRN)) {
+                    System.out.println("올바른 주민번호를 입력하세요!");
+                    continue;
+                }
+                if(!(userRRN.equals(storedRRN))) {
+                    System.out.println("존재하지 않는 주민번호입니다.");
+                    return;
+                }
+                else break;
+            }
+
             System.out.println("============================================");
 
-            if (userRRN.equals(storedRRN)) {
-                // 주민등록번호 일치
-                String storedAccount = userDao.findUserToAccount(loggedInUserId);
-                int balance = accountDao.getBalance(storedAccount);
+            String storedAccount = userDao.findUserToAccount(loggedInUserId);
+            int balance = accountDao.getBalance(storedAccount);
 
-                System.out.println("계좌 조회 결과:");
-                System.out.println("계좌 번호: " + storedAccount);
-                System.out.println("현재 잔액: ₩ " + balance);
+            System.out.println("계좌 조회 결과:");
+            System.out.println("계좌 번호: " + storedAccount);
+            System.out.println("현재 잔액: ₩ " + balance);
 
-            } else {
-                System.out.println("정보가 일치하지 않습니다.");
-            }
         }
         catch (Exception e){
             e.getMessage();
--- src/check/CheckManager.java
+++ src/check/CheckManager.java
@@ -36,35 +36,33 @@ public class CheckManager {
                 return;
             }
 
-            System.out.println("\n\n[계좌 및 예ㆍ적금 조회 서비스]");
-            System.out.println("============================================");
-            System.out.println("계좌 또는 예ㆍ적금을 조회합니다.");
-            System.out.println("============================================");
-            System.out.println("[1] 계좌 조회 서비스");
-            System.out.println("[2] 예ㆍ적금 조회 서비스");
-            System.out.println("[0] 뒤로가기");
-            System.out.println("============================================");
-
             while(true) {
+                System.out.println("\n\n[계좌 및 예ㆍ적금 조회 서비스]");
+                System.out.println("============================================");
+                System.out.println("계좌 또는 예ㆍ적금을 조회합니다.");
+                System.out.println("============================================");
+                System.out.println("[1] 계좌 조회 서비스");
+                System.out.println("[2] 예ㆍ적금 조회 서비스");
+                System.out.println("[0] 뒤로가기");
+                System.out.println("============================================");
                 System.out.print("선택하실 서비스 번호를 입력하세요 (0-2): ");
-                int menuNum = scanner.nextInt();
 
-                switch (menuNum){
-                    case 1:
+                String selected = scanner.nextLine();
+                switch (selected){
+                    case "1":
                         //계좌 조회
                         checkAccountService.checkAccount(loggedInUserId);
                         break;
-                    case 2:
+                    case "2":
                         //예적금 조회
                         checkDepositService.checkDeposit(loggedInUserId);
                         break;
-                    case 0:
-                        break;
+                    case "0":
+                        return;
                     default:
                         System.out.println("원하는 메뉴의 숫자만을 입력해 주세요.");
                         break;
                 }
-                break;
             }
         } catch (IOException e) {
             System.out.println("계좌 조회 중 오류가 발생했습니다: " + e.getMessage());
