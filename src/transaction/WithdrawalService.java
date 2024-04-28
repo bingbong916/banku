1d32c248e5c4584ca0b4aa0ff9a76a95d8e8fbad
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-04-29 08:26:38 +0900

    fix: 출금 금액 1000원 제한 로직 삭제

--- src/transaction/WithdrawalService.java
+++ src/transaction/WithdrawalService.java
@@ -48,9 +48,6 @@ public class WithdrawalService {
                 if (!money.matches("\\d+")) {
                     System.out.println("올바른 양식이 아닙니다.");
                     continue;
-                } else if (Long.parseLong(money)>=0 && Long.parseLong(money) < 1000){
-                    System.out.println("최소 금액 1000원 이상 입력해주세요.");
-                    continue;
                 }
 
                 // 출금 금액 입력 받기
